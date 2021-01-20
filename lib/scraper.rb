b62de2c2d6b461b6e0741d389c71f9ebcbf086df
Dewey <hammersdrichard@gmail.com>
2021-01-20 12:53:15 -0800

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,38 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    arr = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_link = student.attr('href')
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        arr << {name: student_name, location: student_location, profile_url: student_link}
+      end
+    end
+    arr
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    profile = Nokogiri::HTML(open(profile_url))
+    links = profile.css(".social-icon-container").children.css("a").map do |item|
+      item.attr('href')
+    end
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end 
+    student[:bio] = profile.css("div.bio-content.content-holder div.description-holder p").text if profile.css("div.bio-content.content-holder div.description-holder p")
+    student[:profile_quote] = profile.css(".profile-quote").text if profile.css(".profile-quote")
+    student
   end
-
 end
-
