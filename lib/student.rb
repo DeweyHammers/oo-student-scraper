3bfaccc7fa1086df960e8728284df8e5d191e634
Dewey <hammersdrichard@gmail.com>
2021-01-20 12:53:11 -0800

    commit tree e56fc4d8325198dc6155dfb3d5a2cbc732206478

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,24 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    set_attrs(student_hash)
+    @@all << self 
+  end
+
+  def set_attrs(hash)
+    hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.map {|attr| Student.new(attr)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    set_attrs(attributes_hash)
   end
 
   def self.all
-    
+    @@all
   end
 end
 
